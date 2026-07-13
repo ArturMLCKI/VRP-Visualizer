6f776cf0143b16f571a71a0a4ccfaf152253e1aa
ArturMLCKI <maleckiartur00@gmail.com>
2026-07-14 00:56:27 +0200

    Implement basic Flask app structure with address input form and map integration

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../frontend")
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "<p>Initial test</p>"
+
+    x = 53.342686
+    y = -6.267118
+    address = "Dublin Castle"
+
+
+    if request.method == "POST":
+    
+        try:
+            address = request.form["address"]
+            x = float(f"{float(request.form['x']):.3f}")
+            y = float(f"{float(request.form['y']):.3f}")
+        except ValueError:
+            return "Invalid input values", 400
+
+
+    return render_template(
+        "home.html",
+        x=x,
+        y=y,
+        address=address
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
