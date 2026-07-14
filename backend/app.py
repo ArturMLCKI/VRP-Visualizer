7679ca6087ed367d3d9c1c58f05be57f74d15bbe
ArturMLCKI <maleckiartur00@gmail.com>
2026-07-14 19:44:03 +0200

    Refactor address input form to include separate fields for start and destination addresses, and update map rendering logic accordingly

--- backend/app.py
+++ backend/app.py
@@ -6,26 +6,36 @@ app = Flask(__name__, template_folder="../frontend")
 @app.route("/", methods=["GET", "POST"])
 def home():
 
-    x = 53.342686
-    y = -6.267118
-    address = "Dublin Castle"
+    start_x = 53.342686
+    start_y = -6.267118
+    start_address = "Dublin Castle"
 
+    destination_x = 53.349805
+    destination_y = -6.260310
+    destination_address = "Trinity College Dublin"
 
     if request.method == "POST":
-    
+
         try:
-            address = request.form["address"]
-            x = float(f"{float(request.form['x']):.3f}")
-            y = float(f"{float(request.form['y']):.3f}")
+            start_address = request.form["start_address"]
+            start_x = round(float(request.form["start_x"]), 3)
+            start_y = round(float(request.form["start_y"]), 3)
+
+            destination_address = request.form["destination_address"]
+            destination_x = round(float(request.form["destination_x"]), 3)
+            destination_y = round(float(request.form["destination_y"]), 3)
+
         except ValueError:
             return "Invalid input values", 400
 
-
     return render_template(
         "home.html",
-        x=x,
-        y=y,
-        address=address
+        start_x=start_x,
+        start_y=start_y,
+        start_address=start_address,
+        destination_x=destination_x,
+        destination_y=destination_y,
+        destination_address=destination_address
     )
 
 
